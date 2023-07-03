aa4e0489638899222593ef31eb4527a8f89751fe
Shubham Upadhyay <shubhamup47@gmail.com>
2023-07-03 17:13:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+const fontSizeInput = document.getElementById("fontsize")
+const fontColorInput = document.getElementById("fontcolor")
+
+function set(){
+	let size = fontSizeInput.value;
+	let color = fontColorInput.value;
+	localStorage.setItem('size', size)
+	localStorage.setItem('color', color);
+
+	apply();
+}
+
+function apply(){
+	let s = localStorage.getItem('size');
+	let c = localStorage.getItem('color');
+
+	document.body.style.fontSize = `${s}px`;
+	document.body.style.color = c;
+}
+
+document.addEventListener('submit', (event)=>{
+	event.preventDefault();
+	set();
+})
+
+window.addEventListener('load', ()=>{
+	apply();
+})
