28dea2ba41249190c1e157f6aa8ff36451c2534a
Shubham Upadhyay <shubhamup47@gmail.com>
2023-07-05 15:43:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 const fontSizeInput = document.getElementById("fontsize")
 const fontColorInput = document.getElementById("fontcolor")
 
-let cookies = document.cookie ;
 
 function set(){
   let fValue = fontSizeInput.value;
   let cValue = fontColorInput.value;
 
   let cookieString = fValue + '=' + cValue;
-
   let sCookie = cookieString.split("=")
+document.cookie = sCookie;
 
   document.body.style.fontSize = `${sCookie[0]}px`;
-  console.log( document.body.style.fontSize = `${sCookie[0]}px`)
   document.body.style.color = sCookie[1];
-  console.log(document.body.style.color = sCookie[1])
 }
 document.addEventListener('submit', (event)=>{
 	event.preventDefault();
